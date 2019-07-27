7d73c3ee78fb0e0661f7a11d18a4e6a4460437ae
Niveria <52418061+Niveria@users.noreply.github.com>
2019-07-27 15:50:20 +0300

    Задание 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
-//Инициализация слайдера для slider2 
-$(document).on('DOMContentLoaded', function () {
+$(document).ready(function () {
+  //Инициализация слайдера для slider2 
   $('.js-slider').slick({
     infinite: true,
     slidesToShow: 3,
@@ -9,10 +9,8 @@ $(document).on('DOMContentLoaded', function () {
     adaptiveHeight: true,
     //autoplay: true,
     autoplaySpeed: 3000
-  });
-}); // всплывание текста и картинок
+  }); //всплывание текста и картинок
 
-$(document).ready(function () {
   $('.inline-popups').magnificPopup({
     type: 'inline'
   });
@@ -23,5 +21,54 @@ $(document).ready(function () {
     image: {
       verticalFit: true
     }
+  }); //отложенная загрузка 
+
+  $('.js-lazy').each(function (index, el) {
+    var $image = $(el);
+    var realSrc = $image.attr('data-src');
+    $image.attr('src', realSrc);
   });
+}); //ленивая заргузка
+
+var images = document.querySelectorAll('.img-container'); //вот это посылается чтобы вызвать функцию обработки когда изобюражение попало на экран
+
+var options = {
+  rootMargin: '0px',
+  threshold: 0.1
+}; //замена изображения. Прследний пункт который я вставлял делая эту ленивую загрузку. копипаст. вроде бы читаешь код и понятно. и в тоже время непонятно...
+
+var fetchImage = function fetchImage(url) {
+  console.log(url);
+  return new Promise(function (resolve, reject) {
+    var image = new Image();
+    image.src = url;
+    image.onload = resolve;
+    image.onerror = reject;
+  });
+}; //загрузка, вызывает замену изображения
+
+
+var loadImage = function loadImage(image) {
+  var src = image.dataset.src;
+  fetchImage(src).then(function () {
+    // console.log(src)
+    image.src = src;
+  });
+}; //это уэе само действие, вызывает загрузку
+
+
+var handleIntersection = function handleIntersection(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > 0) {
+      console.log(entry.intersectionRatio);
+      loadImage(entry.target);
+    }
+  });
+}; //это вызов действия в зависимости от "опции"
+
+
+var observer = new IntersectionObserver(handleIntersection, options); //вызов заеркпляется за всеми изображениеями... хотелось бы переделать, чтобы только к тем что мне нужны, надо разобраться....
+
+images.forEach(function (img) {
+  observer.observe(img);
 });
